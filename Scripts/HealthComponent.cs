dfcb951746ceab63052632674969a5e915cc91d8
Angelo Maynigo <angelomaynigo@gmail.com>
2024-12-04 10:23:10 -0800

    Added a player health bar

--- Scripts/HealthComponent.cs
+++ Scripts/HealthComponent.cs
@@ -3,25 +3,35 @@ using System;
 
 public partial class HealthComponent : Node2D
 {
-	[Export] private float health = 10.0f;
+	private float currentHealth;
+	[Export] private float maxHealth = 10.0f;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		currentHealth = maxHealth;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (GetParent().IsInGroup("Player")) {
+			TextureProgressBar bar = GetNode<TextureProgressBar>("CanvasLayer/TextureProgressBar");
+			Label label = GetNode<Label>("CanvasLayer/Label");
+			
+			bar.MaxValue = maxHealth;
+			bar.Value = currentHealth;
+			label.Text = currentHealth.ToString() + "/" + maxHealth.ToString();
+		}
 	}
 
 	// Called by a hitbox component
 	public void Damage(float damageNumber)
 	{
-		health -= damageNumber;
+		currentHealth -= damageNumber;
 
 		// Destroy the object
-		if (health <= 0.0f) {
+		if (currentHealth <= 0.0f) {
 			GetParent().QueueFree();
 		}
 	}
